75afcb43d60221e59826f15d9f67bf1f30679da4
unknown <aishiteru1509@gmail.com>
2023-01-05 10:41:12 +0700

    updated

---
+++ Models/SliderItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace BlogSiteWithUmbraco.Models
+{
+    public class SliderItemModel
+    {
+        public int SliderImage { get; set; }
+        public string SliderTitle { get; set; }
+        public string FirstLineText { get; set; }
+        public string ShortDescription { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
